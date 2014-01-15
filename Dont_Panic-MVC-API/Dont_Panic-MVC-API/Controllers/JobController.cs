56a5ddc67f0dcbb582a331d0e546ab55d7cbb49a
Captain Sloth <yhchun1992@hotmail.com>
2014-01-16 11:06:48 +1300

    added job controller, createjob, viewjob

---
+++ Dont_Panic-MVC-API/Dont_Panic-MVC-API/Controllers/JobController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace Dont_Panic_MVC_API.Controllers
+{
+    public class JobController : Controller
+    {
+        //
+        // GET: /Job/
+        public ActionResult CreateJob()
+        {
+            return View();
+        }
+        //
+        // GET: /Job/
+        public ActionResult ViewJob()
+        {
+            return View();
+        }
+
+
+	}
+}
