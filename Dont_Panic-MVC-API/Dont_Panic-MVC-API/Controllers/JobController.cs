f8a67d856c3e6d8b0429e8c740e7f80b17d1fc27
Captain Sloth <yhchun1992@hotmail.com>
2014-01-16 17:18:19 +1300

    Added job controller and views

--- Dont_Panic-MVC-API/Dont_Panic-MVC-API/Controllers/JobController.cs
+++ Dont_Panic-MVC-API/Dont_Panic-MVC-API/Controllers/JobController.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 
 namespace Dont_Panic_MVC_API.Controllers
 {
@@ -6,17 +10,90 @@ namespace Dont_Panic_MVC_API.Controllers
     {
         //
         // GET: /Job/
-        public ActionResult CreateJob()
+        public ActionResult Index()
         {
             return View();
         }
+
         //
-        // GET: /Job/
-        public ActionResult ViewJob()
+        // GET: /Job/Details/5
+        public ActionResult Details(int id)
         {
             return View();
         }
 
+        //
+        // GET: /Job/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
 
-	}
+        //
+        // POST: /Job/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                //When job form is filled out and the post job button is pressed
+                //This is where we post the job details to the database.
+                
+                return RedirectToAction("Index");//Then redirect them to somewhere else.
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Job/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Job/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Job/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Job/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
 }
---
+++ Dont_Panic-MVC-API/Dont_Panic-MVC-API/Models/JobModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Dont_Panic_MVC_API.Models
+{
+    public class JobModel
+    {
+        public string title { get; set; }
+        public string description { get; set; }
+        public string city { get; set; }
+        public string jobtype { get; set; }
+    }
+
+    public class JobContext : DbContext
+    {
+        public DbSet<JobModel> Jobs { get; set; }
+    }
+}
