c3641a3c98e9457965b28995c86666bb14543934
adambates <batesadam@myvuw.ac.nz>
2014-01-29 12:58:45 +1300

    Updated DropdownModels

--- Dont_Panic-MVC-API/Dont_Panic-MVC-API/Models/DropDownModels.cs
+++ Dont_Panic-MVC-API/Dont_Panic-MVC-API/Models/DropDownModels.cs
@@ -44,14 +44,14 @@ namespace Dont_Panic_MVC_API.Models
         {
             List<SelectListItem> listItems = new List<SelectListItem>();
 
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Carterton " });
-            listItems.Add(new SelectListItem() { Value = "2", Text = "Kapiti Coast" });
-            listItems.Add(new SelectListItem() { Value = "3", Text = "Lower Hutt" });
-            listItems.Add(new SelectListItem() { Value = "4", Text = "Masterton" });
-            listItems.Add(new SelectListItem() { Value = "5", Text = "Porirua" });
+            listItems.Add(new SelectListItem() { Value = "1", Text = "Carterton "      });
+            listItems.Add(new SelectListItem() { Value = "2", Text = "Kapiti Coast"    });
+            listItems.Add(new SelectListItem() { Value = "3", Text = "Lower Hutt"      });
+            listItems.Add(new SelectListItem() { Value = "4", Text = "Masterton"       });
+            listItems.Add(new SelectListItem() { Value = "5", Text = "Porirua"         });
             listItems.Add(new SelectListItem() { Value = "6", Text = "South Wairarapa" });
-            listItems.Add(new SelectListItem() { Value = "7", Text = "Upper Hutt" });
-            listItems.Add(new SelectListItem() { Value = "8", Text = "Wellington" });
+            listItems.Add(new SelectListItem() { Value = "7", Text = "Upper Hutt"      });
+            listItems.Add(new SelectListItem() { Value = "8", Text = "Wellington"      });
 
             DistrictList = new SelectList(listItems, "Value", "Text");
         }
@@ -67,84 +67,72 @@ namespace Dont_Panic_MVC_API.Models
         {
             List<SelectListItem> listItems = new List<SelectListItem>();
 
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Aro Valley" });
-            listItems.Add(new SelectListItem() { Value = "2", Text = "Berhampore" });
-            listItems.Add(new SelectListItem() { Value = "3", Text = "Breaker Bay" });
-            listItems.Add(new SelectListItem() { Value = "4", Text = "Broadmeadows" });
-            listItems.Add(new SelectListItem() { Value = "5", Text = "Brooklyn" });
-            listItems.Add(new SelectListItem() { Value = "6", Text = "Churton Park" });
-            listItems.Add(new SelectListItem() { Value = "7", Text = "Crofton Downs" });
-            listItems.Add(new SelectListItem() { Value = "8", Text = "Evans Bay" });
-            listItems.Add(new SelectListItem() { Value = "9", Text = "Glenside" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Grenada North" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Grenada Village" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Hataitai" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Highbury" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Horokiwi" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Houghton Bay" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Island Bay" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Johnsonville" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Kaiwharawhara" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Karaka Bays" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Karori" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Kelburn" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Khandallah" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Kilbirnie" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Kingston" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Kowhai Park" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Lambton" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Lyall Bay" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Makara-Ohariu" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Maupuia" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Melrose" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Miramar" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Moa Point" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Mornington" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Mt Cook" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Mt Victoria" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Newlands" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Newtown" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Ngaio" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Ngauranga" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Northland" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Ohariu" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Oriental Bay" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Owhiro Bay" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Paparangi" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Pipitea" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Rongotai" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Roseneath" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Seatoun" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Southgate" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Strathmore Park" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Takapu Valley" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Tawa" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Te Aro" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Thorndon" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Vogeltown" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Wadestown" });
-            listItems.Add(new SelectListItem() { Value = "1", Text = "Aro Valley" });
+            listItems.Add(new SelectListItem() { Value = "1",  Text = "Aro Valley"         });
+            listItems.Add(new SelectListItem() { Value = "2",  Text = "Berhampore"         });
+            listItems.Add(new SelectListItem() { Value = "3",  Text = "Breaker Bay"        });
+            listItems.Add(new SelectListItem() { Value = "4",  Text = "Broadmeadows"       });
+            listItems.Add(new SelectListItem() { Value = "5",  Text = "Brooklyn"           });
+            listItems.Add(new SelectListItem() { Value = "6",  Text = "Churton Park"       });
+            listItems.Add(new SelectListItem() { Value = "7",  Text = "Crofton Downs"      });
+            listItems.Add(new SelectListItem() { Value = "8",  Text = "Evans Bay"          });
+            listItems.Add(new SelectListItem() { Value = "9",  Text = "Glenside"           });
+            listItems.Add(new SelectListItem() { Value = "10", Text = "Grenada North"      });
+            listItems.Add(new SelectListItem() { Value = "11", Text = "Grenada Village"    });
+            listItems.Add(new SelectListItem() { Value = "12", Text = "Hataitai"           });
+            listItems.Add(new SelectListItem() { Value = "13", Text = "Highbury"           });
+            listItems.Add(new SelectListItem() { Value = "14", Text = "Horokiwi"           });
+            listItems.Add(new SelectListItem() { Value = "15", Text = "Houghton Bay"       });
+            listItems.Add(new SelectListItem() { Value = "16", Text = "Island Bay"         });
+            listItems.Add(new SelectListItem() { Value = "17", Text = "Johnsonville"       });
+            listItems.Add(new SelectListItem() { Value = "18", Text = "Kaiwharawhara"      });
+            listItems.Add(new SelectListItem() { Value = "19", Text = "Karaka Bays"        });
+            listItems.Add(new SelectListItem() { Value = "20", Text = "Karori"             });
+            listItems.Add(new SelectListItem() { Value = "21", Text = "Kelburn"            });
+            listItems.Add(new SelectListItem() { Value = "22", Text = "Khandallah"         });
+            listItems.Add(new SelectListItem() { Value = "23", Text = "Kilbirnie"          });
+            listItems.Add(new SelectListItem() { Value = "24", Text = "Kingston"           });
+            listItems.Add(new SelectListItem() { Value = "25", Text = "Kowhai Park"        });
+            listItems.Add(new SelectListItem() { Value = "26", Text = "Lambton"            });
+            listItems.Add(new SelectListItem() { Value = "27", Text = "Lyall Bay"          });
+            listItems.Add(new SelectListItem() { Value = "28", Text = "Makara-Ohariu"      });
+            listItems.Add(new SelectListItem() { Value = "29", Text = "Maupuia"            });
+            listItems.Add(new SelectListItem() { Value = "30", Text = "Melrose"            });
+            listItems.Add(new SelectListItem() { Value = "31", Text = "Miramar"            });
+            listItems.Add(new SelectListItem() { Value = "32", Text = "Moa Point"          });
+            listItems.Add(new SelectListItem() { Value = "33", Text = "Mornington"         });
+            listItems.Add(new SelectListItem() { Value = "34", Text = "Mt Cook"            });
+            listItems.Add(new SelectListItem() { Value = "35", Text = "Mt Victoria"        });
+            listItems.Add(new SelectListItem() { Value = "36", Text = "Newlands"           });
+            listItems.Add(new SelectListItem() { Value = "37", Text = "Newtown"            });
+            listItems.Add(new SelectListItem() { Value = "38", Text = "Ngaio"              });
+            listItems.Add(new SelectListItem() { Value = "39", Text = "Ngauranga"          });
+            listItems.Add(new SelectListItem() { Value = "40", Text = "Northland"          });
+            listItems.Add(new SelectListItem() { Value = "41", Text = "Ohariu"             });
+            listItems.Add(new SelectListItem() { Value = "42", Text = "Oriental Bay"       });
+            listItems.Add(new SelectListItem() { Value = "43", Text = "Owhiro Bay"         });
+            listItems.Add(new SelectListItem() { Value = "44", Text = "Paparangi"          });
+            listItems.Add(new SelectListItem() { Value = "45", Text = "Pipitea"            });
+            listItems.Add(new SelectListItem() { Value = "46", Text = "Rongotai"           });
+            listItems.Add(new SelectListItem() { Value = "47", Text = "Roseneath"          });
+            listItems.Add(new SelectListItem() { Value = "48", Text = "Seatoun"            });
+            listItems.Add(new SelectListItem() { Value = "49", Text = "Southgate"          });
+            listItems.Add(new SelectListItem() { Value = "50", Text = "Strathmore Park"    });
+            listItems.Add(new SelectListItem() { Value = "51", Text = "Takapu Valley"      });
+            listItems.Add(new SelectListItem() { Value = "52", Text = "Tawa"               });
+            listItems.Add(new SelectListItem() { Value = "53", Text = "Te Aro"             });
+            listItems.Add(new SelectListItem() { Value = "54", Text = "Thorndon"           });
+            listItems.Add(new SelectListItem() { Value = "55", Text = "Vogeltown"          });
+            listItems.Add(new SelectListItem() { Value = "56", Text = "Wadestown"          });
+            listItems.Add(new SelectListItem() { Value = "57", Text = "Wellington Central" });
+            listItems.Add(new SelectListItem() { Value = "58", Text = "Wilton"             });
+            listItems.Add(new SelectListItem() { Value = "59", Text = "Woodridge"          });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            SuburbList = new SelectList(listItems, "Value", "Text");
         }
 
         [Display(Name = "Suburb")]
         public int ActionId { get; set; }
-        public SelectList DistrictList { get; set; } 
+        public SelectList SuburbList { get; set; } 
     }
 
 
