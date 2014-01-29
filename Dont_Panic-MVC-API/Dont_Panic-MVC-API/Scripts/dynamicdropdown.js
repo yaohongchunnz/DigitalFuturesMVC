cd64c9af4ad3e9f804779e789d6fefd15ee6feca
Captain Sloth <yhchun1992@hotmail.com>
2014-01-29 15:42:19 +1300

    javascript prac

---
+++ Dont_Panic-MVC-API/Dont_Panic-MVC-API/Scripts/dynamicdropdown.js
@@ -0,0 +1,17 @@
+﻿function createOption(value) {
+    el = document.createElement('option');
+    el.value = value;
+    el.innerHTML = value;
+    el.id = value;
+    document.getElementById('district').appendChild(el);
+}
+
+document.getElementById('city').addEventListener('click', function () {
+    document.getElementById('district').innerHTML = '';
+    var e = document.getElementById("city");
+    var str = e.options[e.selectedIndex].text;
+    if (str == "Wellington") {
+        createOption("Lower Hutt");
+        createOption("Upper Hutt");
+    }   
+});
