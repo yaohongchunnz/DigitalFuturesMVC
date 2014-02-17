3b67b9490c5fa8a01c321e18bcae93b33b59700b
Captain Sloth <yhchun1992@hotmail.com>
2014-02-18 11:34:03 +1300

    browse and job index improved

--- Dont_Panic-MVC-API/Dont_Panic-MVC-API/Scripts/Areas.js
+++ Dont_Panic-MVC-API/Dont_Panic-MVC-API/Scripts/Areas.js
@@ -62,5 +62,17 @@
         });
     }
 
-                       
+                     
+    function goImgWin(myImage,myWidth,myHeight, origLeft,origTop) {
+        myHeight += 24;
+        myWidth += 24;
+        TheImgWin = window.open(myImage,'image','height=' +
+        myHeight + ',width=' + myWidth +
+        ',toolbar=no,directories=no,status=no,' +
+        'menubar=no,scrollbars=no,resizable=no');
+        TheImgWin.resizeTo(myWidth+2,myHeight+30);
+        TheImgWin.moveTo(origLeft,origTop);
+        TheImgWin.focus();
+        alert("Clicked");
+    }
                     
